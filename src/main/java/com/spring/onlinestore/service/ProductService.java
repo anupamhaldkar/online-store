8012d96884118b167a965dbd719bd206fa4c88ae
anupamhaldkar <anupam.haldkar@outlook.com>
2024-02-19 18:24:35 +0530

    order service added

---
+++ src/main/java/com/spring/onlinestore/service/ProductService.java
@@ -0,0 +1,40 @@
+package com.spring.onlinestore.service;
+
+package com.yourcompany.ecommerce.service;
+
+import com.spring.onlinestore.model.Product;
+import com.spring.onlinestore.repository.ProductRepository;
+import com.yourcompany.ecommerce.model.Product;
+import com.yourcompany.ecommerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteProductById(Long id) {
+        productRepository.deleteById(id);
+    }
+}
